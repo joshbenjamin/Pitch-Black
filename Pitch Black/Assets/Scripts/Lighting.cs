40fec7ebd0b2466daabf309c17a44b4c3994b786
Pietersielie <41905461+Pietersielie@users.noreply.github.com>
2018-08-07 14:49:42 +0200

    Shelves loaded and added.

---
+++ Pitch Black/Assets/Scripts/Lighting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighting : MonoBehaviour {
+
+    [SerializeField]
+    GameObject [] lights;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            foreach (GameObject name in lights)
+                name.GetComponent<Light>().enabled = true;                     
+        }
+        if (Input.GetKey(KeyCode.DownArrow)){
+            foreach (GameObject name in lights)
+                name.GetComponent<Light>().enabled = false;
+        }
+
+
+    }
+}
